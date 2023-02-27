e722a7b4418f73ea1c76a8f30cd3bc84820a94af
Daan Middendorp <git-d@landgenoot.com>
2023-02-27 22:47:13 +0100

    Download photo and resize working

--- photoframe.go
+++ photoframe.go
@@ -1,14 +1,26 @@
 package main
 
+// #cgo pkg-config: MagickWand
+// #include <wand/MagickWand.h>
+import "C"
+
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"math"
 	"math/rand"
 	"net/http"
 	"os"
 	"path"
 	"time"
+)
+
+var (
+	inputFile  = flag.String("in", "test.png", "The file to read in")
+	outputFile = flag.String("out", "test2.png", "The file to write out")
+	scale      = flag.Float64("scale", 0.5, "how much to scale the image by")
 )
 
 func main() {
@@ -23,6 +35,36 @@ func main() {
 	fmt.Println(photoRequest.URL.String())
 	fmt.Println(cookie)
 	downloadPhoto(*photoRequest, "test.png")
+
+	C.MagickWandGenesis()
+
+	// Create a wand
+	mw := C.NewMagickWand()
+
+	defer func() {
+		// Tidy up
+		if mw != nil {
+			C.DestroyMagickWand(mw)
+		}
+
+		C.MagickWandTerminus()
+	}()
+
+	C.MagickReadImage(mw, C.CString(*inputFile))
+
+	// Resize it
+	height := float64(C.MagickGetImageHeight(mw))
+	width := float64(C.MagickGetImageWidth(mw))
+
+	newHeight := math.Round(math.Max(height*(*scale), 1))
+	newWidth := math.Round(math.Max(width*(*scale), 1))
+
+	// Resize the image using the Lanczos filter
+	// The blur factor is a "double", where > 1 is blurry, < 1 is sharp
+	C.MagickResizeImage(mw, C.uint(newWidth), C.uint(newHeight), C.LanczosFilter, 1)
+
+	// Write the new image
+	C.MagickWriteImage(mw, C.CString(*outputFile))
 }
 
 func getRandomPhoto(album synoFotoBrowseItem) (Photo, error) {
